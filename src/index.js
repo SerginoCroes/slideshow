9a29fd49a7490ab8e955be36c9e6afd3caf0e7af
w1z13 <115799235+w1z13@users.noreply.github.com>
2023-03-02 15:46:03 +0100

    finished

--- src/index.js
+++ src/index.js
@@ -2,51 +2,39 @@ const slider = document.querySelector('.slider');
 const leftButton = document.querySelector('.left');
 const rightButton = document.querySelector('.right');
 const dotContainer = document.querySelector('.dotcontainer');
-
 const imgCount = slider.childElementCount;
 let position = 0;
 let timeOut;
 
 leftButton.addEventListener('click', () => {
-    if (position > 0) {
-        position -= 1;
-        resetTime();
-        draw();
-    }
+    if (position > 0) moveImg(position -= 1);
 });
 
 rightButton.addEventListener('click', () => {
-    if (position < imgCount - 1) {
-        position += 1;
-        resetTime();
-        draw();
-    }
+    if (position < imgCount - 1) moveImg(position += 1);
 });
 
 for (let i = 0; i < imgCount; i++) {
     const dot = document.createElement('div');
     dot.classList.add('dot');
-    dot.addEventListener('click', (e) => {
-        position = i;
-        resetTime();
-        draw();
-    });
+    dot.addEventListener('click', () => moveImg(i));
     dotContainer.appendChild(dot);
+}
+
+function moveImg(i) {
+    clearTimeout(timeOut);
+    position = i;
+    draw();
+    nextSlide();
 }
 
 function draw() {
     slider.style.transform = `translate(${-position * 800}px, 0px)`;
-
-    dotContainer.childNodes.forEach(element => {
-        element.style.transform = 'scale(1)';
-        element.style.margin = '6px'
-        element.style.backgroundColor = 'rgba(200, 200, 200, 0.8';
+    dotContainer.childNodes.forEach((element, index) => {
+        element.style.transform = index === position ?  'scale(1.5)' : 'scale(1)';
+        element.style.margin = index === position ? '6px 12px' : '6px';
+        element.style.backgroundColor = index === position ? 'rgba(255, 255, 255, 1' : 'rgba(200, 200, 200, 0.8';
     });
-
-    dotContainer.childNodes[position].style.transform = 'scale(1.5)';
-    dotContainer.childNodes[position].style.margin = '6px 12px'
-    dotContainer.childNodes[position].style.backgroundColor = 'rgba(255, 255, 255, 1';
-
     leftButton.style.opacity = position < 1 ? '0' : '1';
     rightButton.style.opacity = position >= (imgCount - 1) ? '0' : '1';
 }
@@ -61,11 +49,6 @@ function nextSlide() {
         draw();
         nextSlide();
     }, 5000);
-}
-
-function resetTime() {
-    clearTimeout(timeOut);
-    nextSlide();
 }
 
 draw();
